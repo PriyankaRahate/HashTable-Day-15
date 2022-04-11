fce7473c501baae2b43d34b7bd99f6373654400f
unknown <priyanka.rahate23@gmail.com>
2022-04-11 14:49:23 +0530

    FrequencyOfWordinSentence

---
+++ src/main/java/hashtable/HashtableImpl.java
@@ -0,0 +1,58 @@
+package hashtable;
+
+public class HashtableImpl<T1, T2> {
+	
+	    MyMapNode head;
+	    MyMapNode tail;
+
+	    public void add(T1 key, T2 value) {
+	        MyMapNode<T1, T2> myNode = (MyMapNode<T1, T2>) searchNode(key);
+	        if(myNode == null) {
+	            myNode = new MyMapNode<T1, T2>(key , value);
+	            this.append(myNode);
+	        }
+	        else {
+	            myNode.setValue(value);
+	        }
+	    }
+
+	    public void append(MyMapNode<T1, T2> myNode) {
+	        if(this.head == null)
+	            this.head = myNode;
+	        if(this.tail == null)
+	            this.tail = myNode;
+	        else {
+	            this.tail.setNext(myNode);
+	            this.tail = myNode;
+	        }
+	    }
+
+	    public MyMapNode<T1, T2> searchNode(T1 data) {
+	        MyMapNode currentNode = head;
+	        int position = 0;
+	        while (currentNode != null) {
+	            position++;
+	            if (currentNode.getKey().equals(data)) {
+	                return currentNode;
+	            }
+	            currentNode = currentNode.getNext();
+	        }
+	        return currentNode;
+	    }
+
+	    public T2 get(T1 word) {
+	        MyMapNode<T1, T2> myMapNode = searchNode(word);
+	        return (myMapNode == null) ? null : myMapNode.getValue();
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Linked List Nodes { " + head + " }";
+	    }
+
+	    public void printNodes() {
+	        System.out.println("My nodes: " + head);
+	    }
+	}
+
+
--- src/main/java/hashtable/MyMapNode.java
+++ src/main/java/hashtable/MyMapNode.java
@@ -1,5 +1,48 @@
 package hashtable;
 
-public class MyMapNode {
+public class MyMapNode<T1, T2> {
+	T1 key;
+    T2 value;
+    MyMapNode<T1, T2> next;
 
+    public MyMapNode (T1 key, T2 value) {
+        this.key = key;
+        this.value = value;
+        next = null;
+    }
+
+    public T1 getKey() {
+        return key;
+    }
+
+    public void setKey(T1 key) {
+        this.key = key;
+    }
+
+    public T2 getValue() {
+        return this.value;
+    }
+
+    public void setValue(T2 value) {
+        this.value = value;
+    }
+
+    public MyMapNode<T1, T2> getNext() {
+        return next;
+    }
+
+    public void setNext(MyMapNode<T1, T2> next) {
+        this.next = (MyMapNode<T1, T2>) next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder nodeString = new StringBuilder();
+        nodeString.append("Node{" + "K=").append(key).append(" V=").append(value).append("}");
+        if(next != null)
+            nodeString.append("->").append(next);
+        return nodeString.toString();
+    }
 }
+
+
